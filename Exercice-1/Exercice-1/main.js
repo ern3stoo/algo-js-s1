f212e27ab3e2e7a2a80dea87c4c37d2e0f5341d7
ern3stoo <146094069+ern3stoo@users.noreply.github.com>
2023-09-26 17:46:07 +0200

    alding exercice

---
+++ Exercice-1/Exercice-1/main.js
@@ -0,0 +1,16 @@
+var a = 4
+var b = 4
+var c = 3
+
+if (a+b) {
+    console.log ("cest l'égal")
+} 
+else {
+    console.log("Erreur")
+}
+if ( c < b && a+c !=3) {
+    console.log("ok")
+}
+else{
+    console.log("raté")
+}
---
+++ learning/main.js
@@ -0,0 +1,10 @@
+alert("bonjour, tu veux quitter ce site")
+
+console.log("bienvenue dans la console")
+
+var cardName = "Choipeau"
+var age = 12
+var list = []
+var objet = {}
+
+age = age
